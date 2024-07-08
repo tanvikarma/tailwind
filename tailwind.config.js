18482adb957ddc50c92f3912ba9125c9caece1a7
tanvikarma <tanvi@shriffle.com>
2024-07-08 19:16:20 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    colors: {
+      'lightBlue': '#1aa2ac',
+      'yellow': '#f8ce47',
+      'blackForText': '#b0adab',
+
+    },
+    spacing: {
+      'fontSize': '8px'
+    },
+  },
+}
