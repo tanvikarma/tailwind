5abe44fe3c4cc228450ce3f4309c168934d075b8
tanvikarma <tanvi@shriffle.com>
2024-07-15 19:10:21 +0530

    added

---
+++ custom_css/src/js/script.js
@@ -0,0 +1,18 @@
+// function open(){
+//     console.log('called');
+    // document.getElementById('on').style.display = "block";
+    // document.getElementById("navbar").style.display = "none";
+// }
+// function close(){
+//     console.log('called');
+    // document.getElementById('on').style.display = "none";
+    // document.getElementById("navbar").style.display = "block";
+// }
+function hello(){
+   document.getElementById('on').style.display = "block";
+   document.getElementById("navbar").style.display = "none";
+}
+function cross(){
+    document.getElementById('on').style.display = "none";
+   document.getElementById("navbar").style.display = "block";
+}
